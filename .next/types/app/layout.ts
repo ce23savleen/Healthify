235e9843d80ac10f26f286556623cb4ca4e81e2f
ce23savleen <savleenkaur429@gmail.com>
2026-04-02 07:31:04 +0530

    chore: stop tracking .next build files

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Rahul\Downloads\miniproject\Healthify\app\layout.tsx
+// File: C:\Users\Savleen Kaur\OneDrive\Desktop\Healthify all versions\Healthify(GitHub)\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Rahul\Downloads\miniproject\Healthify\app\page.tsx
+// File: C:\Users\Savleen Kaur\OneDrive\Desktop\Healthify all versions\Healthify(GitHub)\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
